a97b993a0f030545d50f374c498a6e4686dd13a2
Nitin Kumar Singh <nitinksingh11@gmail.com>
2016-12-15 21:26:40 +0530

    table schema and views are defined

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,9 +1,34 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+/* Deletes the database if it exists to avoid any errors */
+DROP DATABASE IF EXISTS tournament;
+
+/* Create the database tournament */
+CREATE DATABASE tournament;
+
+/* Connect to the database */
+\c tournament;
+
+CREATE TABLE players (
+        p_id serial PRIMARY KEY,
+        name varchar (25) NOT NULL
+);
+
+CREATE TABLE matches (
+        m_id serial PRIMARY KEY,
+        winner integer REFERENCES players(p_id) NOT NULL,
+        loser integer REFERENCES players(p_id) NOT NULL
+);
 
 
+CREATE VIEW standings AS
+SELECT players.p_id, players.name,
+(SELECT count(matches.winner)
+    FROM matches
+    WHERE players.p_id = matches.winner)
+    AS total_wins,
+(SELECT count(matches.m_id)
+    FROM matches
+    WHERE players.p_id = matches.winner
+    OR players.p_id = matches.loser)
+    AS total_matches
+FROM players
+ORDER BY total_wins DESC, total_matches DESC;
